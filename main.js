26f1956e623ff831f73645129cac953f172ca796
Vaibhav Pancholi <71831734+vaibhav202@users.noreply.github.com>
2023-02-20 16:37:15 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+var btn = document.getElementById("menuToggler");
+var menuClosingSpace = document.getElementById("space");
+var navLinks = document.getElementById("navLinks");
+var first = document.getElementById("top");
+var middle = document.getElementById("middle");
+var bottom = document.getElementById("bottom");
+btn.addEventListener('click', menuToggler);
+menuClosingSpace.addEventListener('click', menuToggler);
+function menuToggler() {
+    if(btn.classList.contains("sm:visible") == true) {
+        //For opening the menu
+        navLinks.classList.toggle("translate-x-full");
+        //Changing menu icon to look a like close icon
+        first.classList.toggle("translate-y-1");
+        first.classList.toggle("-rotate-45");
+        middle.classList.toggle("hidden");
+        bottom.classList.toggle("translate-y-0");
+        bottom.classList.toggle("rotate-45");
+    }
+    else {
+        //For closing the menu
+        navLinks.classList.toggle("translate-x-full");
+
+        //Falling back to default icon
+        first.classList.toggle("translate-y-1");
+        first.classList.toggle("-rotate-45");
+        middle.classList.toggle("hidden");
+        bottom.classList.toggle("translate-y-0");
+        bottom.classList.toggle("rotate-45");
+    }
+}
